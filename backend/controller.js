053816045325894088b8613d7520825650cf4f9a
Ken Okerlund <ken.okerlund@gmail.com>
2023-08-10 18:40:16 -0600

    db design changed, seed function adjusted

--- backend/controller.js
+++ backend/controller.js
@@ -1,22 +1,25 @@
-require('dotenv').config();
-const {CONNECTION_STRING} = process.env;
-const { getValue } = require('@testing-library/user-event/dist/utils');
-const Sequelize = require('sequelize');
+require("dotenv").config();
+const { CONNECTION_STRING } = process.env;
+const { getValue } = require("@testing-library/user-event/dist/utils");
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('', {
+const sequelize = new Sequelize(
+  "",
+  {
     dialect: "postgres",
     dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false
-        }
-    }
-});
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 
 //Seed the database
 const seed = (req, res) => {
-    sequelize.query(`
-    drop table if exists ingredients;
-    drop table if exists instructions;
+  sequelize
+    .query(
+      `
     drop table if exists recipes;
     drop table if exists users;
 
@@ -33,93 +36,136 @@ const seed = (req, res) => {
         recipe_image VARCHAR NOT NULL,
         time_to_make VARCHAR,
         servings INT,
-        FOREIGN KEY (user_id) REFERENCES users(user_id)
-    );
-
-    create table ingredients (
-        ingredient_id SERIAL PRIMARY KEY NOT NULL,
-        recipe_id INT NOT NULL,
         ingredient_quantity VARCHAR NOT NULL,
-        FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id)
-    );
-
-    create table instructions (
-        instruction_id SERIAL PRIMARY KEY NOT NULL,
-        recipe_id INT NOT NULL,
-        step_number INT,
-        instruction VARCHAR,
-        FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id)
+        instruction VARCHAR NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
     );
 
         insert into users(username, password)
         VALUES
         ('SpicyCracka', 'KindKelsier10!');
 
-        insert into recipes(user_id, recipe_name, recipe_image, time_to_make, servings)
+        insert into recipes(user_id, recipe_name, recipe_image, time_to_make, servings, ingredient_quantity, instruction)
         VALUES
-        (1, 'Chocolate Chip Cookies', 'https://images.aws.nestle.recipes/original/5b069c3ed2feea79377014f6766fcd49_Original_NTH_Chocolate_Chip_Cookie.jpg', '10 Minutes', 36);
-
-        insert into ingredients(recipe_id, ingredient_quantity)
-        VALUES
-        (1, '1 Cup Salted Butter'),
-        (1, '1 Cup white sugar'),
-        (1, '1 Cup packed brown sugar'),
-        (1, '2 tsp vanilla extract'),
-        (1, '2 Large Eggs'),
-        (1, '3 Cups Flour'),
-        (1, '1 tsp Baking Soda'),
-        (1, '1/2 tsp Baking Powder'),
-        (1, '1 tsp sea salt'),
-        (1, '2 cups Chocolate Chips');
-
-        insert into instructions(recipe_id, step_number, instruction)
-        VALUES
-        (1, 1, 'Preheat oven to 375 degrees F. Line a baking pan with parchment paper and set aside.'),
-        (1, 2, 'In a separate bowl mix flour, baking soda, salt, baking powder. Set aside.'),
-        (1, 3, 'Cream together butter and sugars until combined.'),
-        (1, 4, 'Beat in eggs and vanilla until fluffy.'),
-        (1, 5, 'Mix in the dry ingredients until combined.'),
-        (1, 6, 'Add 12 oz package of chocolate chips and mix well.'),
-        (1, 7, 'Roll 2-3 TBS (depending on how large you like your cookies) of dough at a time into balls and place them evenly spaced on your prepared cookie sheets.'),
-        (1, 8, 'Bake in preheated oven for approximately 8-10 minutes. Take them out when they are just BARELY starting to turn brown.'),
-        (1, 9, 'Let them sit on the baking pan for 2 minutes before removing to cooling rack.');
-    `).then(() => {
-        console.log('DB SEEDED!')
-        res.sendStatus(200)
-    }).catch(err => console.log('Error seeding the DB', err));
+        (1, 'Chocolate Chip Cookies', 'https://images.aws.nestle.recipes/original/5b069c3ed2feea79377014f6766fcd49_Original_NTH_Chocolate_Chip_Cookie.jpg', '10 Minutes', 36, '[
+            {
+                "quantity": 1,
+                "ingredient": "Cup Salted Butter"
+            },
+            {
+                "quantity": 1,
+                "ingredient": "Cup White Sugar"
+            },
+            {
+                "quantity": 1,
+                "ingredient": "Cup packed Brown Sugar"
+            },
+            {
+                "quantity": 2,
+                "ingredient": "Tsp Vanilla Extract"
+            },
+            {
+                "quantity": 2,
+                "ingredient": "Large Eggs"
+            },
+            {
+                "quantity": 3,
+                "ingredient": "Cups Flour"
+            },
+            {
+                "quantity": 1,
+                "ingredient": "Tsp Baking Soda"
+            },
+            {
+                "quantity": 1/2,
+                "ingredient": "Tsp Baking Powder"
+            },
+            {
+                "quantity": 1,
+                "ingredient": "Tsp Sea Salt"
+            },
+            {
+                "quantity": 2,
+                "ingredient": "Cups Chocolate Chips"
+            }
+        ]', 
+        '[
+            {
+                "instructionStep": 1,
+                "instructionText": "Preheat oven to 375 degrees F. Line a baking pan with parchment paper and set aside."
+            },
+            {
+                "instructionStep": 2,
+                "instructionText": "In a separate bowl mix flour, baking soda, salt, baking powder. Set aside."
+            },
+            {
+                "instructionStep": 3,
+                "instructionText": "Cream together butter and sugars until combined."
+            },
+            {
+                "instructionStep": 4
+                "instructionText": "Beat in eggs and vanilla until fluffy."
+            },
+            {
+                "instructionStep": 5,
+                "instructionText": "Mix in the dry ingredients until combined."
+            },
+            {
+                "instructionStep": 6,
+                "instructionText": "Add 12 oz package of chocolate chips and mix well."
+            },
+            {
+                "instructionStep": 7,
+                "instructionText": "Roll 2-3 TBS (depending on how large you like your cookies) of dough at a time into balls and place them evenly spaced on your prepared cookie sheets."
+            },
+            {
+                "instructionStep": 8,
+                "instructionText": "Bake in preheated oven for approximately 8-10 minutes. Take them out when they are just BARELY starting to turn brown."
+            },
+            {
+                "instructionStep": 9,
+                "instructionText": "Let them sit on the baking pan for 2 minutes before removing to cooling rack."
+            }
+        ]');
+    `)
+    .then(() => {
+      console.log("DB SEEDED!");
+      res.sendStatus(200);
+    })
+    .catch((err) => console.log("Error seeding the DB", err));
 };
 
 // This is to test queries in postman
 function getTest(req, res) {
-    sequelize.query(`SELECT * FROM instructions`)
-    .then(dbRes => res.status(200).send(dbRes[0]))
-    .catch(err => console.log(err));
-};
+  sequelize
+    .query(`SELECT * FROM recipes`)
+    .then((dbRes) => res.status(200).send(dbRes[0]))
+    .catch((err) => console.log(err));
+}
 
 //get the recipe image and name (for the recipe card) that belong to the current user.
 function getRecipeCard(req, res) {
-    sequelize.query(`SELECT recipe_image, recipe_name
+  sequelize
+    .query(
+      `SELECT recipe_image, recipe_name, ingredient_quantity, instruction
     FROM recipes
-    WHERE user_id = 1;`)
-    .then(dbRes => res.status(200).send(dbRes[0]))
-    .catch(err => console.log(err));
-};
-
-
-
-
-module.exports = {
-    seed,
-    getTest,
-    getRecipeCard
+    WHERE user_id = 1;`
+    )
+    .then((dbRes) => res.status(200).send(dbRes[0]))
+    .catch((err) => console.log(err));
 }
 
+module.exports = {
+  seed,
+  getTest,
+  getRecipeCard,
+};
 
 // const instructions = ['1/2 cup butter', '3 handfulls of sugar (powdered)'];
 // // ---
 // // [1][cup of butter]
 // // ---
-// // [ + ] 
+// // [ + ]
 
 // const payloadThatisGoinToTheBe = [
 //     {
