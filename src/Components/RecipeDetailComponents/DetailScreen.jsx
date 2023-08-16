608aabf0ac76adb98827b8b8a14b6e1cbfcbb1d4
Ken Okerlund <ken.okerlund@gmail.com>
2023-08-15 20:05:43 -0600

    detail screen now has all data. Needs styling

--- src/Components/RecipeDetailComponents/DetailScreen.jsx
+++ src/Components/RecipeDetailComponents/DetailScreen.jsx
@@ -15,22 +15,46 @@ const DetailScreen = () => {
                 setRecipe(res.data[0]);
                 console.log(res.data[0]);
             }).finally(() => {
-                setIsLoading(false)
+                setIsLoading(false);
             })
-    }, []);
+    }, [id]);
     // id may need to be a dependency if recipes will be changed before the component is unmounted
 
-    // if (isLoading) {
-    //     return <>Loading!...</>;
-    // }
-    
+
+    if (isLoading) {
+        return <>Loading...</>;
+    }
+
     return (
-        <section>
-            <DetailImage recipe_image={recipe.recipe_image}/>
-            {!isLoading && recipe.instruction.map((instr, i) => {
-                return <li key={i}>{instr.instructionText}</li>
-            })}
-        </section>
+        !isLoading && <div>
+            <div>
+                <DetailImage recipe_image={recipe.recipe_image} />
+                <h4>{recipe.recipe_name}</h4>
+                <div>
+                    <button className='math-button'>1</button>
+                    <button className='math-button'>2</button>
+                    <button className='math-button'>3</button>
+                </div>
+            </div>
+            <div>
+                <div className='ingredients'>
+                    <h2>Ingredients:</h2>
+                    {recipe.quantity_ingredient.map((ing, i) => {
+                        return <li key={i}>{ing.quantity} {ing.ingredient}</li>
+                    })}
+                </div>
+                <div className='instructions'>
+                    <h2>Instructions:</h2>
+                    {recipe.instruction.map((instr, i) => {
+                        return <p key={i}>{i + 1}{"."} {instr.instructionText}</p>
+                    })}
+                </div>
+            </div>
+            <div>
+                <h6>Time: {recipe.time_to_make}</h6>
+                <h6>Servings: {recipe.servings}</h6>
+            </div>
+        </div>
     )
 }
 
